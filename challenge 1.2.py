3fa70e35555205cad5469e1e0f601101111a57b4
41141122213002 <144508536+41141122213002@users.noreply.github.com>
2023-09-21 21:28:32 +0530

    Add files via upload

---
+++ challenge 1.2.py
@@ -0,0 +1,20 @@
+#Leap year
+
+"""
+year 96 4 == 0 &
+year 96 100! = 0/
+year 96 400 == 0
+
+"""
+def isLeapYear(year):
+  if(year 96 4 == 0 and year 96 100 ! = 0) or year 96 400 == 0:
+    return True
+else:
+    return False
+
+year = int(input("Enter a year:"))
+
+if is LeapYear(year):
+     print('{} is a leap year.'.format(year))
+else:
+     print('{} is a leap year.'.format(year))
